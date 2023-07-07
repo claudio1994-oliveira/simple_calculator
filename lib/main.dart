80ea4822d83fa3c0be726b7bba0c607a04393df5
Cláudio Oliveira <franciscoclaudiooliveira@gmail.com>
2023-07-06 21:34:47 -0300

    Fazendo ajustes no layot do app

--- lib/main.dart
+++ lib/main.dart
@@ -13,23 +13,47 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     TextField num1 = const TextField(
-      keyboardType: TextInputType.number,
-    );
+        keyboardType: TextInputType.number,
+        style: TextStyle(
+          fontSize: 20.0,
+          color: Colors.blueGrey,
+        ),
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'Número 1',
+        ));
 
     TextField num2 = const TextField(
-      keyboardType: TextInputType.number,
-    );
+        keyboardType: TextInputType.number,
+        style: TextStyle(
+          fontSize: 20.0,
+          color: Colors.blueGrey,
+        ),
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'Número 2',
+        ));
 
     ElevatedButton button = ElevatedButton(
+      style: ButtonStyle(
+        foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+      ),
       onPressed: () {},
-      child: const Text('Add'),
+      child: const Text('Soma'),
     );
+
+    Padding separator = Padding(padding: const EdgeInsets.all(8.0));
 
     Column columns = Column(
       children: [
         num1,
+        separator,
         num2,
-        button,
+        separator,
+        SizedBox(
+          width: double.infinity,
+          child: button,
+        )
       ],
     );
     return MaterialApp(
@@ -37,6 +61,7 @@ class _MyAppState extends State<MyApp> {
         home: Scaffold(
           appBar: AppBar(
             title: const Text('Calculator'),
+            backgroundColor: Colors.blueGrey[900],
           ),
           body: Padding(
             padding: const EdgeInsets.all(16.0),
